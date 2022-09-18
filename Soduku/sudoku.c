2ea143c451f9c9ddc9bd4e455c7a78d26c390c3a
Andrew Avola <76986167+Meatballin@users.noreply.github.com>
2022-09-18 16:16:53 -0700

    Refactor of tokenize2DArray Function
    Able to handle various types of inputs

--- Soduku/sudoku.c
+++ Soduku/sudoku.c
@@ -61,7 +61,7 @@ void tokenize2DArray(const int ROW_SIZE, const int COL_HEIGHT)
     //countOfNumbers used for detecting if two chars from input stream are
     //being read back to back.
     int countOfNumbers = 0;
-    int newLineCounter = 0;
+
     //used for conversion of character --> int
     int charToInt;
 
@@ -72,37 +72,41 @@ void tokenize2DArray(const int ROW_SIZE, const int COL_HEIGHT)
     int col = 0;
     //====================================================================================
 
-    //Handles all storage of values into 2D array
+    
+    //Handles if input starts with blank lines
     charFromInput = getc(stdin);
+    if(charFromInput == '\n')
+    {
+        while(charFromInput == '\n')
+            charFromInput = getc(stdin);
+        ungetc(charFromInput, stdin);
+    }
+    ungetc(charFromInput, stdin);
+    charFromInput = getc(stdin);
+
     while(charFromInput != EOF)
     {
-
-        if(charFromInput == '\n')
-        {
-            newLineCounter++;
-        }
-
         //Execute this IF statement if we read a valid character from stdin through
         //redirection operator
         if(!isspace(charFromInput) && charFromInput != '\n')
         {
-            newLineCounter = 0;
+        
             checkForRowSize++;
             countOfNumbers++;
+
             if(countOfNumbers > 1 || isalpha(charFromInput))
             {
-                printf("\nInvalid input\n");
+                printf("Invalid input\n");
                 exit(1);
             }
 
             sscanf(&charFromInput, "%d", &charToInt);
-
             //Logic to store our converted values into 2D array.
             array.arr[row][col] = charToInt;
             col++;   
         }
-        //Use this to index to the next row of 2D array from struct
-        if(charFromInput == '\n' && newLineCounter == 1)
+        
+        if(charFromInput == '\n')
         {
             //If we don't have a row of size 9, break
             if(checkForRowSize != 9)
@@ -110,27 +114,34 @@ void tokenize2DArray(const int ROW_SIZE, const int COL_HEIGHT)
                 printf("not enough values in row..\n");
                 exit(1);
             }
+
             
-            //if we hit newline and we have a valid number of digits stored in our row, 
-            //then we can reset our row checker variable.
+            while(charFromInput == '\n')
+                charFromInput = getc(stdin);
+
+            if(charFromInput == EOF)
+            {
+                break;
+            }
+        
+            ungetc(charFromInput, stdin); 
+            row++;
+            
             checkForRowSize = 0;
             col = 0;
-            //index to the next row to begin storage of next row of values
-            row++;
         }
-        
-        //after above logic, look for second character from stdin
+
         charFromInput = getc(stdin);
-        
+       
         //valid inputs reset countOfNumbers back to zero
         if(isspace(charFromInput) || charFromInput == '\n')
             countOfNumbers = 0;   
     }
 
     //check if we even have a valud 9x9 matrix
-    if(row != COL_HEIGHT)
+    if(row != COL_HEIGHT - 1)
     {
-        printf("not a valid 9x9 matrix\n");
+        printf("not a valid 9x9 matrix");
         exit(1);
     }
 }
@@ -147,6 +158,7 @@ void print2DArray(struct arrayHandler array, const int ROW_SIZE)
             printf("%d", array.arr[i][j]);
     }  
 }
+
 
 //Function for row checking thread
 void *doThreadRowTesting(void *ptr)
